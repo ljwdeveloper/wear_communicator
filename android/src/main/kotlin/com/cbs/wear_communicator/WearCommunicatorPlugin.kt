2e3aa3a2c8c3e4df82f0a077e264c523b8ecc095
ljwdeveloper <ljwdeveloper@gmail.com>
2024-12-27 15:29:01 +0900

    안드로이드 Wearable 등 모듈 추가, 메시징 관련 코드 추가.

--- android/src/main/kotlin/com/cbs/wear_communicator/WearCommunicatorPlugin.kt
+++ android/src/main/kotlin/com/cbs/wear_communicator/WearCommunicatorPlugin.kt
@@ -1,35 +1,119 @@
 package com.cbs.wear_communicator
 
-import androidx.annotation.NonNull
+import android.content.Context
+import android.util.Log
+import com.google.android.gms.wearable.*
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.EventChannel.StreamHandler
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
+import org.json.JSONObject
+
 /** WearCommunicatorPlugin */
-class WearCommunicatorPlugin: FlutterPlugin, MethodCallHandler {
+class WearCommunicatorPlugin: FlutterPlugin, MethodCallHandler, StreamHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+  private lateinit var methodChannel: MethodChannel
+  private lateinit var eventChannel: EventChannel
+  private var eventSink: EventChannel.EventSink? = null
+  private val tag = "WearCommunicator"
+  private lateinit var context: Context
+
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "wear_communicator")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
-    }
+    methodChannel = MethodChannel(flutterPluginBinding.binaryMessenger, "wear_communicator")
+    eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "wear_communicator_events")
+    methodChannel.setMethodCallHandler(this)
+    eventChannel.setStreamHandler(this)
+    context = flutterPluginBinding.applicationContext
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+    methodChannel.setMethodCallHandler(null)
+    eventChannel.setStreamHandler(null)
   }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    when (call.method) {
+      "sendMessage" -> {
+        val message = call.argument<Map<String, Any>>("message")
+        if (message != null) {
+          sendMessageToConnected(message)
+          result.success(null)
+        } else {
+          result.error("INVALID_ARGUMENT", "Message is null", null)
+        }
+      }
+      "getPlatformVersion" -> {
+        result.success("Android ${android.os.Build.VERSION.RELEASE}")
+      }
+      else -> {
+        result.notImplemented()
+      }
+    }
+  }
+
+  private fun sendMessageToConnected(message: Map<String, Any>) {
+    val messageJson = JSONObject(message).toString()
+    val nodeClient = Wearable.getNodeClient(context)
+    nodeClient.connectedNodes.addOnCompleteListener { task ->
+      if (task.isSuccessful) {
+        val nodes = task.result
+        nodes?.forEach { node ->
+          val messageClient = Wearable.getMessageClient(context)
+          messageClient.sendMessage(node.id, "/message", messageJson.toByteArray())
+            .addOnCompleteListener { sendTask ->
+              if (sendTask.isSuccessful) {
+                Log.d(tag, "Message sent to ${node.displayName}: $messageJson")
+              } else {
+                Log.e(tag, "Failed to send message: ${sendTask.exception}")
+              }
+            }
+        }
+      } else {
+        Log.e(tag, "Failed to get connected nodes: ${task.exception}")
+      }
+    }
+  }
+
+  override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+    eventSink = events
+    startListeningForMessages()
+  }
+
+  override fun onCancel(arguments: Any?) {
+    eventSink = null
+  }
+
+  private fun startListeningForMessages() {
+    val messageClient = Wearable.getMessageClient(context)
+    messageClient.addListener { event ->
+      val message = String(event.data)
+      Log.d(tag, "Message received: $message")
+      try {
+        val messageMap = JSONObject(message).toMap()
+        eventSink?.success(messageMap)
+      } catch (e: Exception) {
+        Log.e(tag, "Failed to parse received message: $message", e)
+      }
+    }
+  }
+
+  private fun JSONObject.toMap(): Map<String, Any> {
+    val map = mutableMapOf<String, Any>()
+    val keys = keys()
+    while (keys.hasNext()) {
+      val key = keys.next()
+      map[key] = this.get(key)
+    }
+    return map
+  }
+
 }
